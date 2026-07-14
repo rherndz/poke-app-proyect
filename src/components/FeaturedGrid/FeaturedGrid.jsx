1454ef1dd7ad341cf4ec55392116687b599f103f
rherndz <roberth48983cds@gmail.com>
2026-07-14 16:27:30 -0600

    fix: design and routing adjustments

--- src/components/FeaturedGrid/FeaturedGrid.jsx
+++ src/components/FeaturedGrid/FeaturedGrid.jsx
@@ -5,13 +5,14 @@ export const FeaturedGrid = ({ pokemons, isLoading }) => {
     <section className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12 mb-8">
       
       {/* header section */}
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="font-black text-xl sm:text-2xl md:text-3xl tracking-wide text-pok-slate-50">
+      <div className="flex justify-between items-center mb-10 border-b-2 border-pok-dark-100 text-pok-dark-100 pb-6">
+        <h2 className="font-black text-xl sm:text-2xl md:text-2xl tracking-wide text-pok-dark-100">
           Pokémon Destacados
         </h2>
-        <button className="inline-flex items-center gap-1 text-xs sm:text-sm font-bold text-pok-slate-50 hover:text-red-500 transition-colors duration-200 cursor-pointer">
-          Ver todos <i className="bi bi-chevron-right text-[10px]"></i>
-        </button>
+        
+        <a className="inline-flex items-center justify-center gap-1 text-xs sm:text-sm font-bold  bg-pok-dark-100 cursor-pointer text-pok-red-400 hover:bg-pok-red-400 hover:text-pok-slate-50 rounded-2xl p-2 ">
+          Ver todos 
+        </a>
       </div>
 
       {/* loading state or content */}
@@ -23,7 +24,8 @@ export const FeaturedGrid = ({ pokemons, isLoading }) => {
           </p>
         </div>
       ) : (
-        /* responsive Grid  */
+        
+        /* grid of featured Pokémon */
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 justify-items-center">
           {pokemons.map((pokemon) => {
             
--- src/components/PokemonGrid/PokemonGrid.jsx
+++ src/components/PokemonGrid/PokemonGrid.jsx
@@ -5,23 +5,24 @@ export const PokemonGrid = ({ pokemons }) => {
     <section className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12 mb-16">
       
       {/* header section */}
-      <div className="flex justify-between items-baseline mb-6 border-b border-pok-slate-50 pb-4">
-        <h2 className="text-white font-sans text-xl font-bold tracking-wide sm:text-2xl">
+      <div className="flex justify-between items-baseline mb-10 border-b-2 border-pok-dark-100 pb-6">
+        <h2 className="font-black text-xl sm:text-2xl md:text-2xl tracking-wide text-pok-dark-100">
           Todos los Pokémon
         </h2>
-        <span className="text-xs font-mono font-bold text-slate-500">
-          {pokemons.length} resultados
-        </span>
+
+         <a className="inline-flex items-center justify-center gap-1 text-xs sm:text-sm font-bold  bg-pok-dark-100 cursor-pointer text-pok-red-400 hover:bg-pok-red-400 hover:text-pok-slate-50 rounded-2xl p-2 ">
+          Ver todos 
+        </a>
       </div>
 
-      {/* grid responvide desing mobile */}
+      {/* grid with all the pokemon  */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 justify-items-center">
         {pokemons.map((pokemon) => (
           <PokemonCard
             key={pokemon.id}
-            id={pokemon.id}
+            id={`#${String(pokemon.id).padStart(3, '0')}`} 
             name={pokemon.name}
-            image={pokemon.image}
+            image={pokemon.sprites?.other?.['official-artwork']?.front_default || pokemon.sprites?.front_default}
             types={pokemon.types}
           />
         ))}
